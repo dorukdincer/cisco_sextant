40d8345c6ad8652823c79c9ed1f18af06391a0ac
dorukdincer <dorukk12@gmail.com>
2023-02-16 19:59:35 -0800

    Task 2 Completed

---
+++ sextant/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Card.css"
+
+export default function Card() {
+    return (
+        <div className='card-wrap'>
+            <h1 className='card-title'>Lorem ipsum</h1>
+            <p1 className='card-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p1>
+        </div>
+    );
+}
---
+++ sextant/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../styles/Header.css"
+
+export default function Header() {
+    return (
+        <header className='website-header'>
+            <h1 className='header-title'>SEXTANT</h1>
+        </header>
+    );
+}
